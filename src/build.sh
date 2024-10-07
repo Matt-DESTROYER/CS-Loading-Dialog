438a5a6ed481bb26a3d1c5b0906c3feafd75fa4f
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 10:29:46 +1100

    `.csproj` and build fixes

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 
+# MUST BE RUN FROM ./src
 
 # detect platform and architecture
 if [[ $RUNNER_OS == "Windows" ]]; then
@@ -6,13 +8,19 @@ if [[ $RUNNER_OS == "Windows" ]]; then
 elif [[ $RUNNER_OS == "Linux" ]]; then
     PLATFORM="linux"
 fi
+
 ARCH=$(uname -m)
 if [[ $ARCH == x86_64* ]]; then
 	ARCH="x64"
 elif [[ $ARCH == i*86 ]]; then
-	ARCH="x32"
+	ARCH="x86"
 elif [[ $arch == arm* ]]; then
 	ARCH="arm"
 fi
 
-dotnet publish -r $PLATFORM-$ARCH
+# ensure the build directory exists
+if [ ! -d "../build" ]; then
+    mkdir ../build
+fi
+
+dotnet publish ~/LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --disable-build-servers --output ../build
