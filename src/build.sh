5b5e68fda8c54014f1018ad57f71a9c87ef8d7a2
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 10:13:11 +1100

    Fix platform

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,11 @@
 
 
-# detect architecture
+# detect platform and architecture
+if [[ $RUNNER_OS == "Windows" ]]; then
+    PLATFORM="win"
+elif [[ $RUNNER_OS == "Linux" ]]; then
+    PLATFORM="linux"
+fi
 ARCH=$(uname -m)
 if [[ $ARCH == x86_64* ]]; then
 	ARCH="x64"
@@ -10,4 +15,4 @@ elif [[ $arch == arm* ]]; then
 	ARCH="arm"
 fi
 
-dotnet publish -r ${RUNNER_OS,,}-$ARCH
+dotnet publish -r $PLATFORM-$ARCH
