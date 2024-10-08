cb070482519d8baf2088c7cc02c2f21eda2f70ac
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-09 07:39:53 +1100

    Fixed build :fingers_crossed:

--- src/demo/demo.cs
+++ src/demo/demo.cs
@@ -5,13 +5,13 @@ using LoadingDialogs;
 class Program {
 	public static void Main() {
 		// create a loading dialog
-		LoadingDialog loadingDlg = new LoadingDialog("Demo loading dialog:", 100, 20);
+		LoadingDialog loadingDlg1 = new LoadingDialog("Demo loading dialog:", 100, 20);
 		// start it
-		loadingDlg.Start();
+		loadingDlg1.Start();
 
 		// every 200 milliseconds increment the progress
 		for (int i = 0; i < 100; i++) {
-			loadingDlg.IncrementProgress();
+			loadingDlg1.IncrementProgress();
 			Thread.Sleep(200);
 		}
 
@@ -19,16 +19,16 @@ class Program {
 		Console.WriteLine("");
 
 		// create a new loading dialog
-		loadingDlg = new LoadingDialog("Another loading dialog!", 100, 30);
+		LoadingDialog loadingDlg2 = new LoadingDialog("Another loading dialog!", 100, 30);
 		// start it
-		loadingDlg.Start();
+		loadingDlg2.Start();
 
 		// every 100 milliseconds set the progress to the current progress
 		for (int i = 1; i <= 100; i += 3) {
-			loadingDlg.SetProgress(i);
+			loadingDlg2.SetProgress(i);
 			Thread.Sleep(100);
 		}
-		loadingDlg.SetProgress(100);
+		loadingDlg2.SetProgress(100);
 
 		// add a new line
 		Console.WriteLine("");
