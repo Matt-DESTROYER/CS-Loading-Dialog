9551123edf10382b69c63ab4cef186e35832ff57
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 20:10:25 +1100

    Attempt auto platform detection if env var not set

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # MUST BE RUN FROM ./src
 
@@ -7,6 +7,18 @@ if [[ $RUNNER_OS == "Windows" ]]; then
     PLATFORM="win"
 elif [[ $RUNNER_OS == "Linux" ]]; then
     PLATFORM="linux"
+else
+	echo Environment variable RUNNER_OS not specified...
+	if [[ $OSTYPE == "linux" ]]; then 
+		RUNNER_OS = "Linux"
+		PLATFORM = "linux"
+	elif [[ $OSTYPE == "msys" || $OSTYPE == "cygwin" ]]; then
+		RUNNER_OS = "Windows"
+		PLATFORM = "win"
+	else
+		echo Failed to autodetect platform... >&2
+		exit 1
+	fi
 fi
 echo Platform detected: $RUNNER_OS
 
