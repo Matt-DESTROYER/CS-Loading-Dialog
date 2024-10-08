5f46c70cc589434ac03a9690b8ab3901ac87d419
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 17:15:10 +1100

    Custom directories for library and demo builds

--- src/build.sh
+++ src/build.sh
@@ -26,11 +26,11 @@ rm -rf ../build/$RUNNER_OS-$ARCH
 
 echo Building library...
 
-dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH
+dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/library
 
 echo Building demo...
 
-dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH
+dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/demo
 
 rm -rf bin
 rm -rf obj
