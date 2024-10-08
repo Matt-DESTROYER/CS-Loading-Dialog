757560186d122e90a42cb97e16b6667978cdef07
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 20:34:11 +1100

    Fix script

--- src/build.sh
+++ src/build.sh
@@ -7,23 +7,26 @@ BUILD_PLATFORMS=("win", "linux", "osx")
 BUILD_ARCHS=("x86", "x64", "arm", "arm64")
 
 # build
+rm -rf ../build
+mkdir ../build
 for i in "${!BUILD_PLATFORMS[@]}"; do
-	RUNNER_OS = ${OS_NAMES[$i]}
-	PLATFORM = ${BUILD_PLATFORMS[$i]}
-	ARCH = ${BUILD_ARCHS[$i]}
+	RUNNER_OS=${OS_NAMES[$i]}
+	PLATFORM=${BUILD_PLATFORMS[$i]}
+
+	for j in "${!BUILD_ARCHS[@]}"; do
+		ARCH=${BUILD_ARCHS[$j]}
 	
-	echo Building for $RUNNER_OS $ARCH...
+		echo Building for $RUNNER_OS $ARCH...
 
-	rm -rf ../build/$RUNNER_OS-$ARCH
+		echo Building library...
+		dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/library
 
-	echo Building library...
-	dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/library
+		echo Building demo...
+		dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/demo
 
-	echo Building demo...
-	dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/demo
-
-	rm -rf bin
-	rm -rf obj
+		rm -rf bin
+		rm -rf obj
+	done
 done
 
 echo Build complete
