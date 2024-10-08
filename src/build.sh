bcbced98509f8be93c0662ecb827ab5fbd395301
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-08 20:29:45 +1100

    Build all with one Ubuntu machine

--- src/build.sh
+++ src/build.sh
@@ -2,49 +2,28 @@
 
 # MUST BE RUN FROM ./src
 
-# detect OS
-if [[ $RUNNER_OS == "Windows" ]]; then
-    PLATFORM="win"
-elif [[ $RUNNER_OS == "Linux" ]]; then
-    PLATFORM="linux"
-else
-	echo Environment variable RUNNER_OS not specified...
-	if [[ $OSTYPE == "linux" ]]; then 
-		RUNNER_OS = "Linux"
-		PLATFORM = "linux"
-	elif [[ $OSTYPE == "msys" || $OSTYPE == "cygwin" ]]; then
-		RUNNER_OS = "Windows"
-		PLATFORM = "win"
-	else
-		echo Failed to autodetect platform... >&2
-		exit 1
-	fi
-fi
-echo Platform detected: $RUNNER_OS
-
-# detect architecture
-ARCH=$(uname -m)
-if [[ $ARCH == x86_64* ]]; then
-	ARCH="x64"
-elif [[ $ARCH == i*86 ]]; then
-	ARCH="x86"
-elif [[ $arch == arm* ]]; then
-	ARCH="arm"
-fi
-echo Architecture detected: $ARCH
+OS_NAMES=("Windows", "Linux", "Mac OS")
+BUILD_PLATFORMS=("win", "linux", "osx")
+BUILD_ARCHS=("x86", "x64", "arm", "arm64")
 
 # build
-rm -rf ../build/$RUNNER_OS-$ARCH
+for i in "${!BUILD_PLATFORMS[@]}"; do
+	RUNNER_OS = ${OS_NAMES[$i]}
+	PLATFORM = ${BUILD_PLATFORMS[$i]}
+	ARCH = ${BUILD_ARCHS[$i]}
+	
+	echo Building for $RUNNER_OS $ARCH...
 
-echo Building library...
+	rm -rf ../build/$RUNNER_OS-$ARCH
 
-dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/library
+	echo Building library...
+	dotnet publish LoadingDialogs.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/library
 
-echo Building demo...
+	echo Building demo...
+	dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/demo
 
-dotnet publish demo.csproj --os $PLATFORM --arch $ARCH -c Release --output ../build/$RUNNER_OS-$ARCH/demo
+	rm -rf bin
+	rm -rf obj
+done
 
-rm -rf bin
-rm -rf obj
-
-echo Build complete.
+echo Build complete
